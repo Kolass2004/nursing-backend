8aa3f530892b8a01e2e15cadf1c922728331a56a
Kolass rexon J <89210243+Kolass2004@users.noreply.github.com>
2024-07-24 16:16:52 +0530

    new

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Enable CORS for all routes
 app.use(cors());
